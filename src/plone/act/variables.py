45982b049050fb6eb4f9345ea531bc53fc4fa83f
Godefroid Chapelle <gotcha@bubblenet.be>
2012-10-13 18:17:26 +0200

    simplify *** Settings *** section

---
+++ src/plone/act/variables.py
@@ -0,0 +1,11 @@
+import os
+
+from plone.app.testing.interfaces import PLONE_SITE_ID
+
+PORT = os.environ["ZSERVER_PORT"]
+ZOPE_URL = "http://localhost:%s" % PORT
+PLONE_URL = "%s/%s" % (ZOPE_URL, PLONE_SITE_ID)
+BROWSER = "Firefox"
+REMOTE_URL = ""
+DESIRED_CAPABILITIES = ""
+TEST_FOLDER = "%s/acceptance-test-folder"
