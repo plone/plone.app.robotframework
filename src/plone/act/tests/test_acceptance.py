cb379f9dd4c889d1d70f58cb05b523df553add26
Godefroid Chapelle <gotcha@bubblenet.be>
2012-10-14 16:30:18 +0200

    work in progress

--- src/plone/act/tests/test_acceptance.py
+++ src/plone/act/tests/test_acceptance.py
@@ -1,6 +1,8 @@
 import unittest
+import os.path
 
 from plone.testing import layered
+from plone.app.testing import PLONE_ZSERVER
 from plone.act.tests.layer import LIVESEARCH_ZSERVER
 
 import robotsuite
@@ -9,7 +11,11 @@ import robotsuite
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
-        layered(robotsuite.RobotTestSuite("acceptance"),
-                layer=LIVESEARCH_ZSERVER),
+        layered(robotsuite.RobotTestSuite(
+            os.path.join("acceptance", "standard")),
+            layer=PLONE_ZSERVER),
+        layered(robotsuite.RobotTestSuite(
+            os.path.join("acceptance", "livesearch")),
+            layer=LIVESEARCH_ZSERVER),
     ])
     return suite
