b4b42a4b18d7417e7cb108b37b84d1cbecb7f8ab
Godefroid Chapelle <gotcha@bubblenet.be>
2012-10-14 11:30:29 +0200

    implicit_wait is configurable from environment

--- src/plone/act/variables.py
+++ src/plone/act/variables.py
@@ -2,9 +2,8 @@ import os
 
 from plone.app.testing.interfaces import PLONE_SITE_ID
 
-PORT = 55001
-if 'ZSERVER_PORT' in os.environ:
-    PORT = os.environ["ZSERVER_PORT"]
+PORT = os.environ.get('ZSERVER_PORT', 55001)
+SELENIUM_IMPLICIT_WAIT = os.environ.get('SELENIUM_IMPLICIT_WAIT', '1s')
 
 ZOPE_URL = "http://localhost:%s" % PORT
 PLONE_URL = "%s/%s" % (ZOPE_URL, PLONE_SITE_ID)
