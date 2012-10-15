ce2e35ef98e3953a3e15eb66ae0327d70baa9bd3
Asko Soukka <asko.soukka@iki.fi>
2012-10-15 19:46:35 +0300

    Increase implicit wait (for Travis-CI).

--- src/plone/act/variables.py
+++ src/plone/act/variables.py
@@ -3,7 +3,7 @@ import os
 from plone.app.testing.interfaces import PLONE_SITE_ID
 
 PORT = os.environ.get('ZSERVER_PORT', 55001)
-SELENIUM_IMPLICIT_WAIT = os.environ.get('SELENIUM_IMPLICIT_WAIT', '1s')
+SELENIUM_IMPLICIT_WAIT = os.environ.get('SELENIUM_IMPLICIT_WAIT', '2s')
 
 ZOPE_URL = "http://localhost:%s" % PORT
 PLONE_URL = "%s/%s" % (ZOPE_URL, PLONE_SITE_ID)
