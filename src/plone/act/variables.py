b272ba6640e7940f9cdd3f87bcad551f47120812
Asko Soukka <asko.soukka@iki.fi>
2012-10-17 19:31:05 +0300

    Added PLONE_SITE_ID configurable with environment variable. (Not for plone.app.testing-based tests, but to override variables for pybot.)

--- src/plone/act/variables.py
+++ src/plone/act/variables.py
@@ -6,6 +6,7 @@ PORT = os.environ.get('ZSERVER_PORT', 55001)
 PORT = os.environ.get('PLONE_TESTING_PORT', PORT)
 SELENIUM_IMPLICIT_WAIT = os.environ.get('SELENIUM_IMPLICIT_WAIT', '1s')
 
+PLONE_SITE_ID = os.environ.get('PLONE_SITE_ID', PLONE_SITE_ID)
 ZOPE_URL = os.environ.get('ZOPE_URL', "http://localhost:%s" % PORT)
 PLONE_URL = os.environ.get('PLONE_URL', "%s/%s" % (ZOPE_URL, PLONE_SITE_ID))
 BROWSER = os.environ.get('BROWSER', "Firefox")
