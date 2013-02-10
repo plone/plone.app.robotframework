e7fad6ede23a6f71ac98bfe24b3e22eda4d7999c
Asko Soukka <asko.soukka@iki.fi>
2013-02-10 10:45:59 +0200

    Add separate test for testing 'Add document'-keyword

--- src/plone/act/tests/test_robot.py
+++ src/plone/act/tests/test_robot.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 import robotsuite
 from plone.act.testing import REMOTE_LIBRARY_FUNCTIONAL_TESTING
+from plone.app.testing import PLONE_ZSERVER
 from plone.testing import layered
 
 
@@ -10,5 +12,7 @@ def test_suite():
     suite.addTests([
         layered(robotsuite.RobotTestSuite("robot_remote.txt"),
                 layer=REMOTE_LIBRARY_FUNCTIONAL_TESTING),
+        layered(robotsuite.RobotTestSuite("robot_add_document.txt"),
+                layer=PLONE_ZSERVER),
     ])
     return suite
