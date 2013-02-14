3b11697bb8eecc39c0a78a7ec375be62d6286794
Asko Soukka <asko.soukka@iki.fi>
2013-02-14 18:00:29 +0200

    Simplify 'RemoteKeywordsLibrary'-example to make it easier to copy it

--- src/plone/act/remote.py
+++ src/plone/act/remote.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from OFS.SimpleItem import SimpleItem
 
+from Products.PluggableAuthService.plugins import DomainAuthHelper
+from Products.PlonePAS.Extensions.Install import activatePluginInterfaces
+
 
 class RemoteKeywordsLibrary(SimpleItem):
     """Robot Framework Remote Library Tool for Plone
@@ -8,20 +11,18 @@ class RemoteKeywordsLibrary(SimpleItem):
     See also: http://robotframework.googlecode.com/hg/doc/userguide/RobotFrameworkUserGuide.html?r=2.7.65#remote-library-interface
 
     """
-
-    def __init__(self, keywords=None):
-        self.keywords = keywords
-
     def get_keyword_names(self):
         """Return names of the implemented keywords
         """
-        names = filter(lambda x: x[0] != '_', dir(self.keywords))
+        blacklist = dir(SimpleItem)
+        blacklist.extend(['get_keyword_names', 'run_keyword'])
+        names = filter(lambda x: x[0] != '_' and x not in blacklist, dir(self))
         return names
 
     def run_keyword(self, name, args):
         """Execute the specified keyword with given arguments.
         """
-        func = getattr(self.keywords, name, None)
+        func = getattr(self, name, None)
         result = {'error': '', 'return': ''}
         try:
             retval = func(*args)
@@ -32,3 +33,39 @@ class RemoteKeywordsLibrary(SimpleItem):
             result['status'] = 'PASS'
             result['return'] = retval
         return result
+
+    def product_has_been_activated(self, product_name):
+        """Asserts that given product_name is activated in
+        portal_quickinstaller.
+
+        """
+        from Products.CMFCore.utils import getToolByName
+        quickinstaller = getToolByName(self, "portal_quickinstaller")
+        assert quickinstaller.isProductInstalled(product_name),\
+            "Product '%s' was not installed." % product_name
+
+    def autologin_as(self, *args):
+        """Adds and configures DomainAuthHelper PAS-plugin to login
+        all anonymous users from localhost as a special *Remote User* with
+        one or more given roles. Examples of use::
+
+            Autologin as  Manager
+            Autologin as  Site Administrator
+            Autologin as  Member  Contributor
+
+        """
+        if "robot_login" in self.acl_users.objectIds():
+            self.acl_users.robot_login._domain_map.clear()
+        else:
+            DomainAuthHelper.manage_addDomainAuthHelper(
+                self.acl_users, "robot_login")
+            activatePluginInterfaces(self, "robot_login")
+        self.acl_users.robot_login.manage_addMapping(
+            match_type="equals", match_string="localhost", roles=args)
+
+    def logout_autologin(self):
+        """Clears DomainAuthHelper's map to effectively 'logout' user
+        after 'autologin_as'.
+        """
+        if "robot_login" in self.acl_users.objectIds():
+            self.acl_users.robot_login._domain_map.clear()
--- src/plone/act/testing.py
+++ src/plone/act/testing.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from plone.app.testing import (
     PloneSandboxLayer,
     applyProfile,
@@ -38,9 +39,7 @@ class RemoteKeywordsLibraryLayer(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE,)
 
     def setUpPloneSite(self, portal):
-        remote_keywords = RemoteKeywords(portal)
-        remote_library = RemoteKeywordsLibrary(remote_keywords)
-        portal._setObject("RemoteKeywordsLibrary", remote_library)
+        portal._setObject("RemoteKeywordsLibrary", RemoteKeywordsLibrary())
 
     def tearDownPloneSite(self, portal):
         portal._delObject("RemoteKeywordsLibrary")
@@ -52,39 +51,3 @@ REMOTE_LIBRARY_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(REMOTE_LIBRARY_FIXTURE, z2.ZSERVER_FIXTURE),
     name="RemoteKeywordsLibrary:Functional"
 )
-
-
-from Products.PluggableAuthService.plugins import DomainAuthHelper
-from Products.PlonePAS.Extensions.Install import activatePluginInterfaces
-
-
-class RemoteKeywords(object):
-
-    def __init__(self, portal):
-        self._portal = portal
-
-    def product_has_been_activated(self, product_name):
-        from Products.CMFCore.utils import getToolByName
-        quickinstaller = getToolByName(self._portal, "portal_quickinstaller")
-        assert quickinstaller.isProductInstalled(product_name),\
-            "Product '%s' was not installed." % product_name
-
-    def autologin_as(self, *args):
-        """Adds and configures DomainAuthHelper PAS-plugin to login
-        all anonymous users from localhost as a special *Remote User* with
-        one or more given roles. Examples of use::
-
-            Autologin as  Manager
-            Autologin as  Site Administrator
-            Autologin as  Member  Contributor
-
-        """
-
-        if "robot_login" in self._portal.acl_users.objectIds():
-            self._portal.acl_users.robot_login._domain_map.clear()
-        else:
-            DomainAuthHelper.manage_addDomainAuthHelper(
-                self._portal.acl_users, "robot_login")
-            activatePluginInterfaces(self._portal, "robot_login")
-        self._portal.acl_users.robot_login.manage_addMapping(
-            match_type="equals", match_string="localhost", roles=args)
