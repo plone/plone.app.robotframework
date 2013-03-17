5116af73b6ba577865432855489fc20302b2b2b3
Asko Soukka <asko.soukka@iki.fi>
2013-03-17 16:45:30 +0200

    Fix the default ZOPE_HOST to 0.0.0.0 to support ie

--- src/plone/app/robotframework/variables.py
+++ src/plone/app/robotframework/variables.py
@@ -7,7 +7,7 @@ PORT = os.environ.get('ZSERVER_PORT', 55001)
 PORT = os.environ.get('PLONE_TESTING_PORT', PORT)
 SELENIUM_IMPLICIT_WAIT = os.environ.get('SELENIUM_IMPLICIT_WAIT', '1s')
 
-ZOPE_HOST = os.environ.get('ZOPE_HOST', "localhost")
+ZOPE_HOST = os.environ.get('ZOPE_HOST', "0.0.0.0")
 ZOPE_URL = os.environ.get('ZOPE_URL', "http://%s:%s" % (ZOPE_HOST, PORT))
 PLONE_SITE_ID = os.environ.get('PLONE_SITE_ID', PLONE_SITE_ID)
 PLONE_URL = os.environ.get('PLONE_URL', "%s/%s" % (ZOPE_URL, PLONE_SITE_ID))
