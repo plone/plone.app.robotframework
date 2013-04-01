7fe423c492b7b878670d0f99baffa2c48ed5076b
Asko Soukka <asko.soukka@iki.fi>
2013-04-01 18:43:20 +0300

    Update setup.py; Strip buildout-ride.cfg to contain only wxPython build instructions; Add ride.cfg to use system wxPython installation with RIDE

--- setup.py
+++ setup.py
@@ -12,44 +12,61 @@ long_description = (
     open('CONTRIBUTORS.txt').read()
     + '\n' +
     open('CHANGES.txt').read()
-    + '\n')
-
+    + '\n'
+)
 
 console_scripts = [
     "robot-server = plone.app.robotframework.server:server",
     "robot = plone.app.robotframework.robotentrypoints:robot",
     "pybot = plone.app.robotframework.robotentrypoints:pybot",
     "rebot = plone.app.robotframework.robotentrypoints:rebot",
-    "act_server = plone.app.robotframework.server:server",  # BBB
+    "act_server = plone.app.robotframework.server:server",  # BBB for plone.act
 ]
 
 entry_points = dict(console_scripts=console_scripts)
 
-setup(name='plone.app.robotframework',
-      version=version,
-      description="Robot Framework testing resources for Plone",
-      long_description=long_description,
-      classifiers=[
-          "Programming Language :: Python",
-      ],
-      keywords='',
-      author='',
-      author_email='',
-      url='https://github.com/plone/plone.act/',
-      license='gpl',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'argparse',
-          'plone.app.testing[robot]>=4.2.2',
-          'zope.deprecation',
-      ],
-      extras_require={'reload': [
-          'watchdog'
-      ]},
-      entry_points=entry_points,
-      )
+install_requires = [
+    'setuptools',
+    # Utility dependencies:
+    'argparse',
+    # Plone testing dependencies:
+    'plone.testing',
+    'plone.app.testing',  # [robot] >= 4.2.2 would include Robot deps.
+    # Functional Robot testing dependencies:
+    'robotsuite',
+    'robotframework',
+    'robotframework-selenium2library',
+    # Implicit Robot Framework dependencies:
+    'decorator',
+    'selenium',
+    # BBB for plone.act:
+    'zope.deprecation',
+]
+
+reload_requires = [
+    # Watch for filesystem changes:
+    'watchdog'
+]
+
+setup(
+    name='plone.app.robotframework',
+    version=version,
+    description="Robot Framework testing resources for Plone",
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='Asko Soukka',
+    author_email='asko.soukka@iki.fi',
+    url='https://github.com/plone/plone.app.robotframework/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    extras_require={'reload': reload_requires},
+    entry_points=entry_points,
+)
