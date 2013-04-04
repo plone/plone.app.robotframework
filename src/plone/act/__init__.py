59491d65902ee8474b6d2c7f904890dc40261212
Asko Soukka <asko.soukka@iki.fi>
2013-04-04 10:02:52 +0300

    Drop BBB for plone.act

--- src/plone/act/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import zope.deprecation
-import plone.app.robotframework
-sys.modules['plone.act'] = zope.deprecation.deprecation.deprecated(
-    plone.app.robotframework,
-    'A module called plone.act is now plone.app.robotframework.'
-)
