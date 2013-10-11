738c3187490d853b83c9c9b55310df55641d1a48
Asko Soukka <asko.soukka@iki.fi>
2013-10-11 17:17:01 +0300

    Fix whitespaces

--- src/plone/app/robotframework/quickinstaller.py
+++ src/plone/app/robotframework/quickinstaller.py
@@ -7,7 +7,6 @@ class QuickInstaller(RemoteLibrary):
     def product_is_activated(self, product_name):
         """Assert that given product_name is activated (installed) in
         portal_quickinstaller.
-
         """
         from Products.CMFCore.utils import getToolByName
         quickinstaller = getToolByName(self, 'portal_quickinstaller')
