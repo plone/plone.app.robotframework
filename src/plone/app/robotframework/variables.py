a6e30e001bbb36b2447cae00a84e1fe4627f9f06
Asko Soukka <asko.soukka@iki.fi>
2013-10-11 17:13:56 +0300

    Refactor to use python only for environment variables and define other variables in robot to support robot variable overrides

--- src/plone/app/robotframework/variables.py
+++ src/plone/app/robotframework/variables.py
@@ -9,12 +9,7 @@ except ImportError:
 # plone.testing checks environment to allow overriding DEFAULT_ZSERVER_PORT
 ZSERVER_PORT = os.environ.get('ZSERVER_PORT', DEFAULT_ZSERVER_PORT)
 
-# Allow specifiying HOST and PORT through environment variables
-# for instance, if tests should go through a proxy
+# Allow specifying HOST and PORT through environment variables for instance,
+# if tests should go through a proxy
 ZOPE_HOST = os.environ.get('ZOPE_HOST', 'localhost')
 ZOPE_PORT = os.environ.get('ZOPE_PORT', ZSERVER_PORT)
-
-ZOPE_URL = 'http://{0}:{1}'.format(ZOPE_HOST, ZOPE_PORT)
-
-PLONE_SITE_ID = 'plone'
-START_URL = PLONE_URL = '/'.join((ZOPE_URL, PLONE_SITE_ID))
--- src/plone/app/robotframework/variables.robot
+++ src/plone/app/robotframework/variables.robot
@@ -2,3 +2,12 @@
 
 Variables  plone/app/testing/interfaces.py
 Variables  plone/app/robotframework/variables.py
+
+*** Variables ***
+
+${ZOPE_URL}  http://${ZOPE_HOST}:${ZOPE_PORT}
+
+${PLONE_SITE_ID}  plone
+${PLONE_URL}  ${ZOPE_URL}/${PLONE_SITE_ID}
+
+${START_URL}  ${PLONE_URL}
