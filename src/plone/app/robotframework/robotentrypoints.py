67b803aa942d1545868193328927d59ed73482db
Asko Soukka <asko.soukka@iki.fi>
2014-02-08 15:46:53 +0100

    Add ignored Sphinx-directives to pybot to make it easier to run pybot agains Sphinx documentation

--- src/plone/app/robotframework/robotentrypoints.py
+++ src/plone/app/robotframework/robotentrypoints.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import sys
 
 from robot import run_cli
@@ -15,6 +14,19 @@ else:
 
 
 def pybot():
+    # This code hides warnings for known Sphinx-only-directives when
+    # executing pybot against Sphinx-documentation:
+    from docutils.parsers.rst.directives import register_directive
+    from docutils.parsers.rst.roles import register_local_role
+    dummy_directive = lambda *args: []
+    options = ('maxdepth', 'creates', 'numbered', 'hidden')
+    setattr(dummy_directive, 'content', True)
+    setattr(dummy_directive, 'options', dict([(opt, str) for opt in options]))
+    register_directive('toctree', dummy_directive)
+    register_directive('robotframework', dummy_directive)
+    register_local_role('ref', dummy_directive)
+
+    # Run pybot
     run_cli(sys.argv[1:])
 
 
@@ -57,7 +69,8 @@ def pybabel():
         ('**.robot', 'plone.app.robotframework.pybabel:extract_robot')
     ])
 
-    # This code hides warnings for missing Sphinx-only-directives:
+    # This code hides warnings for known Sphinx-only-directives when
+    # executing pybot against Sphinx-documentation:
     from docutils.parsers.rst.directives import register_directive
     from docutils.parsers.rst.roles import register_local_role
     dummy_directive = lambda *args: []
