e480b04cfd442be57bb141d9bf1e437534713ce8
Asko Soukka <asko.soukka@iki.fi>
2014-04-02 20:34:36 +0300

    Add 'Get total amount of sent emails'-keyword into MockMailHost remote library

--- src/plone/app/robotframework/mailhost.py
+++ src/plone/app/robotframework/mailhost.py
@@ -8,3 +8,9 @@ class MockMailHost(RemoteLibrary):
         """Return the last sent email from MockMailHost sent messages storage
         """
         return self.MailHost.messages[-1] if self.MailHost.messages else u""
+
+    def get_the_total_amount_of_sent_emails(self):
+        """Return the total amount of sent emails in MockMailHost sent messages
+        storage as integer
+        """
+        return len(self.MailHost.messages)
