9c5cd9de4fdbad2892c548e6987ee032fa9cc982
Maurits van Rees <maurits@vanrees.org>
2016-07-15 18:56:56 +0200

    Preparing release 1.0
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.0 (unreleased)
+1.0 (2016-07-15)
 ----------------
 
 Breaking changes:
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 import sys
 
-version = '1.0.dev0'
+version = '1.0'
 
 
 def indented(filename):
