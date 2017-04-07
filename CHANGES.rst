f708a26bdd8daa4ce0e066519a9b8cb495292a24
Maurits van Rees <maurits@vanrees.org>
2017-04-08 01:33:55 +0200

    Bumped version for feature release.
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,8 @@
 Changelog
 =========
 
-1.0.4 (unreleased)
-------------------
+1.1 (unreleased)
+----------------
 
 Breaking changes:
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 import sys
 
-version = '1.0.4.dev0'
+version = '1.1.dev0'
 
 
 def indented(filename):
